cd1a698ba8e614edf08d7d88b8b2be1bb26cbb3d
Jayanth somuri <69779313+Jayanthsomuri@users.noreply.github.com>
2021-05-11 11:25:26 +0530

    Add files via upload

---
+++ 9-Path-module.js
@@ -0,0 +1,12 @@
+//Path module
+const path= require('path')
+console.log(path.sep)
+
+const filepath=path.join('/contnet/','subfolder','text.txt')
+console.log(filepath)
+
+const base=path.basename(filepath)
+console.log(base)
+
+const absolute=path.resolve(__dirname,'content','subfolder','text.txt');
+console.log(absolute)
