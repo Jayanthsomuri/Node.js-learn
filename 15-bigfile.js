8a662635d3aa4a98339cdf16f84f6e9a8736234d
Jayanth somuri <69779313+Jayanthsomuri@users.noreply.github.com>
2021-05-26 10:07:50 +0530

    Delete 15-bigfile.js

--- 15-bigfile.js
+++
@@ -1,9 +0,0 @@
-//STREAMS
-//Writeable
-//Readable
-//Duplex(both read and write)
-//Transform
-const{writeFileSync} =require('fs')
-for(let i=0;i<10000;i++){
-  writeFileSync('./content/big.txt',`Hello world this is :${i}th time\n`,{flag:'a'})
-}
