aa73e269762eac119259c76680707f60fcedc63c
Jayanth somuri <69779313+Jayanthsomuri@users.noreply.github.com>
2021-05-27 10:17:13 +0530

    Delete 18-http methods.js

--- 18-http methods.js
+++
@@ -1,9 +0,0 @@
-//HTTP methods
-//GET Read  
-//POST insert data 
-//DELETE Delete data 
-//GET-->www.store.com/api/orders ((get all orders))
-//POST-->www.store.com/api/orders (place an order(send data))
-//GET-->www.store.com/api/orders/:id ((get single orders(path)))
-//PUT--->www.store.com/api/orders/:id (update specific order)
-//DELETE -->www.store.com/api/orders/:id  (deletes specific order)
